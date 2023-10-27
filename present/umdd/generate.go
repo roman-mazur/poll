8613cdd2e4ce1ba10d0a00ce88db69c2b3cd4a9e
Roman Mazur <roman@rmazur.io>
2023-10-27 16:26:45 +0200

    present: Update umdd diagram

--- present/umdd/generate.go
+++ present/umdd/generate.go
@@ -1,6 +1,6 @@
 package umdd
 
-//go:generate go run oss.terrastruct.com/d2 -t 1 --sketch case-vote.d2 case-vote.png
-//go:generate go run oss.terrastruct.com/d2 -t 1 --sketch case-annotate.d2 case-annotate.png
-//go:generate go run oss.terrastruct.com/d2 -t 1 --sketch case-fetch.d2 case-fetch.png
-//go:generate go run oss.terrastruct.com/d2 -t 7 umdd-diagram.d2 umdd-diagram.png
+//go:generate d2 -t 1 --sketch case-vote.d2 case-vote.png
+//go:generate d2 -t 1 --sketch case-annotate.d2 case-annotate.png
+//go:generate d2 -t 1 --sketch case-fetch.d2 case-fetch.png
+//go:generate d2 -t 7 umdd-diagram.d2 umdd-diagram.png
