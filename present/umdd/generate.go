f33b83dcdd8148e94653ae044ba05f1df5c55ac0
Roman Mazur <roman@rmazur.io>
2024-10-13 23:54:39 +0200

    present: Add extra images

--- present/umdd/generate.go
+++ present/umdd/generate.go
@@ -3,4 +3,5 @@ package umdd
 //go:generate d2 -t 7 --sketch case-vote.d2 case-vote.png
 //go:generate d2 -t 7 --sketch case-annotate.d2 case-annotate.png
 //go:generate d2 -t 7 --sketch case-fetch.d2 case-fetch.png
+//go:generate d2 -t 7 --sketch umdd-diagram-intro.d2 umdd-diagram-intro.png
 //go:generate d2 -t 7 --sketch umdd-diagram.d2 umdd-diagram.png
