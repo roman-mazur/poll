5fe5e0368af972148c9786301bb050c16110dcea
Roman Mazur <roman@rmazur.io>
2025-09-19 22:29:33 +0200

    build-linux.sh: Build for multiple architectures

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pollsvc-amd64 ./cmd/pollsvc
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pollsvc-x86_64 ./cmd/pollsvc
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o pollsvc-arm64 ./cmd/pollsvc
