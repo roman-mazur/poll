694bbc37667602fc21136fa43643cd76f419abbb
Roman Mazur <roman@rmazur.io>
2026-01-31 22:59:38 +0100

    build-linux: add os suffix

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pollsvc-x86_64 ./cmd/pollsvc
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o pollsvc-arm64 ./cmd/pollsvc
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pollsvc-x86_64-linux ./cmd/pollsvc
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o pollsvc-arm64-linux ./cmd/pollsvc
